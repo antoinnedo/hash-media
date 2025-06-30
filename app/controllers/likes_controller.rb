3376924a7e9df32e92b47d5040f6e4cdec8e5dc4
antoinnedo <anthonydo2307@gmail.com>
2025-06-30 15:49:43 +0700

    add like/ unlike count through query

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,12 @@ class LikesController < ApplicationController
 
   def create
     @post.likes.where(user: current_user).first_or_create
-    redirect_to @post, notice: "You liked this post."
+    redirect_back(fallback_location: root_path, notice: "You liked this post.")
   end
 
   def destroy
     @post.likes.where(user: current_user).destroy_all
-    redirect_to @post, notice: "You unliked this post."
+    redirect_back(fallback_location: root_path, notice: "You unliked this post.")
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_30_072844) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_30_084046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,7 +40,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_30_072844) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
