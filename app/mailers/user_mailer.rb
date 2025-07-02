2563d5604ace67a0e53cd92099dbda3a4013bc84
antoinnedo <anthonydo2307@gmail.com>
2025-07-02 16:25:01 +0700

    Revert "Add my new features and mailer changes"
    This reverts commit b68db1bcd58d329b659e0822ae939aa9aeae596e.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,7 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com' # This can be anything
 
-  def welcome_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Welcome to Hash')
+  def welcome_email
+    mail(to: 'test_recipient@example.com', subject: 'Welcome to My Awesome Site')
   end
 end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get "home/index"
+  devise_for :users
+  root to: "posts#index"
+  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :posts do
+    resources :comments, only: [:create, :update, :destroy]
+    resource :like, only: [:create, :destroy]
+  end
+
+  resources :comments do
+    resources :responses, only: [:create, :update, :destroy]
+  end
+
+  resources :relationships, only: [:create, :destroy]
+
+end
