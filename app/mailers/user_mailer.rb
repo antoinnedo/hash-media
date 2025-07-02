8bb9d066b6eb29ae9750e3c04bcf1a5737c7c8a1
antoinnedo <anthonydo2307@gmail.com>
2025-07-02 16:53:19 +0700

    recovered the mailer and config files

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 # app/mailers/user_mailer.rb
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com' # This can be anything
+  default from: 'hehe@example.com' # This can be anything
 
-  def welcome_email
-    mail(to: 'test_recipient@example.com', subject: 'Welcome to My Awesome Site')
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome to HASH')
   end
 end
